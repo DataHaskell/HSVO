638138ae60008d1837c8782301ba6a2313768483
John Vial <John Vial>
2016-11-24 19:12:45 +0800

    Blergh! Lots of small changes, but it compiles... I'm abit worried that I might have made some poor design choices along time ago. At any rate, this commit contains a bit of a foray into Monad Transformers, as well as the use of lenses.

--- src/Numeric/Algorithms/HSVO/SVM.hs
+++ src/Numeric/Algorithms/HSVO/SVM.hs
@@ -1,3 +1,5 @@
+{- SVM main implementation area -}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Numeric.Algorithms.HSVO.SVM where
@@ -11,12 +13,6 @@ import Control.Monad.Reader --(MonadReader, Reader, ReaderT)
 import Control.Monad.State --(MonadState, State, StateT)
 import Control.Monad.Writer
 
-data TargetVector = TargetVector
-               {
-                 _tsv :: TrainingSupportVector,
-                 _index :: Int
-               }
-makeLenses  ''TargetVector
 
 -- We need to be able to pass both the global parameters, but also some state parameters
 -- in the form of the vectors ....
@@ -26,22 +22,113 @@ makeLenses  ''TargetVector
 -- | The main list of things...
 newtype WorkingState = WorkingState
                      {
-                       _vectorList :: V.Vector TargetVector
+                       _vectorList :: [TrainingSupportVector]
                      }
 
 makeLenses ''WorkingState
 
 
-newtype SVMProblem a = SVMProblem
-                   {
-                     solveProblem :: StateT WorkingState (WriterT String (Reader SVMParameters) ) a
-                   }  deriving (MonadT WorkingState)
+{-newtype SVMProblem a = SVMProblem {
+                         runProb :: StateT Int (WriterT String (Reader Int) ) a
+                         }
+                       deriving (MonadState Int , MonadWriter String, MonadReader Int)
+-}
+type SVMProblem a = WriterT String (ReaderT SVMParameters (State WorkingState)) a
 
-takeStep :: SVMProblem a -> SVMProblem a
-takeStep problem =
+
+takeStepBlah :: SVMProblem ()
+takeStepBlah =
   do
-    vecs <- pure $  (solveProblem problem)
-    problem
+    vecs <- get :: SVMProblem WorkingState
+    params <- ask :: SVMProblem SVMParameters
+    -- <. combines an indexed lens.
+    -- %@= , accesses an indexed lens inside the state monad over a traversal...
+    -- simple case is just re-assigning stuff... should be able to use it to make
+    -- more useful things.
+--    ((vectorList . itraversed <. tsv) <. classError) %@= (\i _ -> (vecs ^. vectorList . traversed . tsv)  !! i)
+    return ()
+
+
+
+
+
+takeStep :: SVMProblem (TrainingSupportVector -> TrainingSupportVector )
+takeStep =
+  do
+    vecs <- get
+    params <- ask
+    pure $  \x -> collateMaybes x [takeStepDetail params vec x  | vec <-  (_vectorList vecs ) ] -- list comprehension to evaluate over all maybes...
+
+helper1 :: Maybe a -> [a] -> [a]
+helper1 (Just a) b = a : b
+helper1 Nothing b = b
+
+maybeToLists :: [Maybe a] -> [a]
+maybeToLists  =
+  foldr (helper1) []
+
+
+
+
+collateMaybes :: TrainingSupportVector -> [Maybe (TrainingSupportVector, TrainingSupportVector)] -> TrainingSupportVector
+collateMaybes tsv1 tsvList =
+  let
+     validResults = WorkingState {_vectorList = snd <$> maybeToLists tsvList}
+     alpha_data = foldrOf (vectorList . traverse . supvec . alpha) (\a (sum, count) -> ( (scalarToDbl a) + sum, count+1)) (0, 0) validResults
+     sum =  fst alpha_data
+     len =  snd alpha_data
+     mean = sum / len
+     tsv = firstOf (vectorList.traversed) validResults :: Maybe TrainingSupportVector
+     newTsv = (\t -> set (supvec . alpha) (wrapScalar mean) t) <$> tsv
+     -- now extract the first element of validResults and modify it so that it has an alpha that is the mean value.
+     -- now compute the new error for the global training example given that.
+
+  in
+    undefined
+   -- We need to use the foldMaybe thing, where it returns two sets of lits
+
+{-
+
+I want a lens:
+ASetter s s a b -> m a -> m b -> m ()
+or maybe m (a -> b)
+
+This would let me access the WorkingState out of the environment.
+
+Ok, what about takeStep being, lets evaluate the desired alpha from all of the vectors.
+Lets then average the contributions, sortof like stochastic gradient descent, except instead
+of randomly doing it we will just eval all of them...
+
+
+
+takeStep se this lets me examine a single element in the list and then modify it. However, I need to be
+examining 2 elements from the lens and modifying both elements afterwards...
+
+so we could try a sort of gibbs sampling style approach where we optimise only one, but calculate the required change
+from all vectors... is this a case of constrained thinking generating creative outputs?
+
+Hmmm... one problem is that we have a need to re-evaluate the SVM given its weights...
+
+
+-}
+
+
+{-
+
+So we can use a list comprehension to express that we want to combine
+the pair-wise associations of each, this will also work for a list of 2 lists that are not equal..
+
+But, how to do this with lenses?
+
+Lets not optimise this... instead lets write a lens that iterates over other lenses.
+I.e. inside the traversal, lets scan through another lens traversal, but in this traversal, lets
+check the svm parameter, and if its ok, go through and try and solve it.
+
+It will not be particularly efficient, as we will need to visit every vector on every loop, even
+though they are not needed. However, perhaps the compiler will help me...
+
+-}
+
 
 
 {-
@@ -56,19 +143,22 @@ http://stackoverflow.com/questions/28587132/making-read-only-functions-for-a-sta
 -}
 
 
-{-
-takeStep :: SVMParameters
+
+
+takeStepDetail :: SVMParameters
             -> TrainingSupportVector
             -> TrainingSupportVector
             -> Maybe (TrainingSupportVector, TrainingSupportVector)
-takeStep params sv1 sv2 =
-    let
+takeStepDetail params sv1 sv2 = Just (sv1, sv2)
+{-
+  let
         x1 = sv1^.supvec.vector
         x2 = sv2^.supvec.vector
         diff = sumVector (elementDifference x1 x2)
         identical = abs (diff) < params^.epsillon
         sVectors = V.map (\a-> a^.supvec) tData
     in
+
         do
             -- First step, check that the vectors are identical.
             _ <- pure (if identical then Nothing else Just ())
@@ -85,8 +175,8 @@ takeStep params sv1 sv2 =
             -- Looks like I will need a complete SVM copy, and to make a
             -- new training set... can I build a traverse?
             return (finalSv1, finalSv2)-- modify existing training vector
-
 -}
+
 {-
 trainToSv :: V.Vector TrainingSupportVector -> V.Vector Sample
 trainToSv = V.map (\a -> a^.supvec.vector )
