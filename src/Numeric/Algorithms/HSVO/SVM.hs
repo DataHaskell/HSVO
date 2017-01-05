9f310d910cc31cae5cc503065cdf7129d3361257
John Vial <John Vial>
2017-01-05 15:35:35 +0800

    Top-bottom connected through types. Ok, I've connected the control logic to the maths using types... I guess all that is left now is implementation...

--- src/Numeric/Algorithms/HSVO/SVM.hs
+++ src/Numeric/Algorithms/HSVO/SVM.hs
@@ -73,33 +73,21 @@ shuffleVectors = error "Implemnet shuffleVectors"
 
 {-| Walk the list and then attempt to improve the SVM. Don't forget to shuffle the list! -}
 solvePairs :: SVMParameters -> [TrainingSupportVector] -> [TrainingSupportVector]
-solvePairs params (x:xs) = fst $ foldl (pairHelper params (x:xs)) ([], Just x) xs
+solvePairs params (x:xs) = fst $ foldl (pairHelper params  (x:xs) ) ([], Just x) xs
 solvePairs _ [] = []
 
 {-| Helper function for solvePairs, it will run over the pairs and determine appropriate output -}
-pairHelper :: SVMParameters -> [TrainingSupportVector]
+pairHelper :: SVMParameters
+           -> [TrainingSupportVector]
            -> ([TrainingSupportVector], Maybe TrainingSupportVector)
            -> TrainingSupportVector
            -> ([TrainingSupportVector], Maybe TrainingSupportVector)
 pairHelper _ _ (examined, Nothing) next =
   (examined, Just next)
 pairHelper params allVecs (examined, Just target) next =
-  let
-    result = optimizePair params allVecs target next
-  in
-    case result of
+    case takeStepDetail params (WorkingState allVecs) target next  of
       Nothing -> ((next:examined), Just target)
       Just (r1, r2) -> (r1: r2:examined, Nothing)
-
-
-{-| Now we have a pair of support vectors, lets optimise them. Note that the two that are selected
-will also be present in the allVecs list -}
-optimizePair :: SVMParameters
-             -> [TrainingSupportVector]
-             -> TrainingSupportVector
-             -> TrainingSupportVector
-             -> Maybe (TrainingSupportVector, TrainingSupportVector)
-optimizePair params allVecs t1 t2  = error "implement optimizePair"
 
 
 -- Note to self, we could use the lens "zoom" to implement the reduced set heuristic
@@ -119,3 +107,55 @@ makeSupportVectors = error "implement makeSupportVectors"
 {-| HighLevel function to predict data given an SVM -}
 predictSVM :: SVMParameters -> [RawFeatures] -> [ClassLabel]
 predictSVM params testData = error "implement predictSVM"
+
+
+
+-- Check to ensure that the Current vectors are not in the
+inSet :: WorkingState -> TrainingSupportVector -> Bool
+inSet _ _ = error "Implement inSet"
+
+maybeInSet :: WorkingState -> TrainingSupportVector -> Maybe ()
+maybeInSet ws sv | (inSet ws sv) == True = Just ()
+maybeInSet _ _ = Nothing
+
+maybePass :: Bool -> Maybe ()
+maybePass True = Just ()
+maybePass False = Nothing
+
+testMaybe :: Bool -> Maybe ()
+testMaybe True = Just ()
+testMaybe False = Nothing
+
+takeStepDetail :: SVMParameters
+            -> WorkingState
+            -> TrainingSupportVector
+            -> TrainingSupportVector
+            -> Maybe (TrainingSupportVector, TrainingSupportVector)
+takeStepDetail params workState sv1 sv2 = --Just (sv1, sv2)
+  let
+        x1 = sv1^.supvec.vector
+        x2 = sv2^.supvec.vector
+        diff = sumVector (elementDifference x1 x2)
+        identical = abs (diff) < params^.epsillon
+
+  in
+        do
+            maybeInSet workState sv1
+            maybeInSet workState sv2
+            maybePass identical
+
+            (a2, a2clip) <- determineAlpha2 params sv1 sv2
+            a1 <- pure $ alpha1New sv1 sv2 (wrapScalar a2) (wrapScalar a2clip)
+
+            sv1' <- pure $ SupportVector {_alpha= a1, _vector=sv1^.supvec.vector}
+            sv2' <- pure $ SupportVector {_alpha=wrapScalar (a2), _vector=sv2^.supvec.vector}
+            -- Since workState doesn't contain sv1 and sv2 we can append them to a temp list and use it here
+            oldSvec <- pure $ focusOnSupportVector workState
+            newSvec <- pure $ concat [[sv1', sv2'], oldSvec]
+
+            pred1 <- pure $ svm params newSvec x1
+            pred2 <- pure $ svm params newSvec x2
+            finalSv1 <- pure $ constructTrainingVec sv1 pred1 sv1'
+            finalSv2 <- pure $ constructTrainingVec sv2 pred2 sv2'
+
+            return (finalSv1, finalSv2)-- modify existing training vector
